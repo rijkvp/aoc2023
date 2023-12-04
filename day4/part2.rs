25d7d42e33049e2d0e73568cf0e53fe90b7de874
rijkvp <rijk@rijkvp.nl>
2023-12-04 08:07:21 +0100

    Pass part1, day 4

---
+++ day4/part2.rs
@@ -0,0 +1,29 @@
+use std::{
+    collections::HashSet,
+    io::{stdin, BufRead, BufReader},
+};
+
+fn main() {
+    let mut cards = Vec::new();
+    for line in BufReader::new(stdin()).lines() {
+        let line = line.unwrap();
+        let (_, nums) = line.split_once(':').unwrap();
+        let (win_nums, own_nums) = nums.split_once('|').unwrap();
+        let win_nums = win_nums
+            .split_whitespace()
+            .map(|s| s.parse::<u64>().unwrap())
+            .collect::<HashSet<u64>>();
+        let own_nums = own_nums
+            .split_whitespace()
+            .map(|s| s.parse::<u64>().unwrap())
+            .collect::<HashSet<u64>>();
+        let mut count = 0;
+        for num in own_nums {
+            if win_nums.contains(&num) {
+                count += 1;
+            }
+        }
+        cards.push(count);
+    }
+    println!("{cards:?}")
+}
