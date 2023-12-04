bfd40605dc8bcce1e34ee8bcec9031aa4c16695f
rijkvp <rijk@rijkvp.nl>
2023-12-04 12:59:39 +0100

    Did second part

--- day4/part2.rs
+++ day4/part2.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 fn main() {
-    let mut cards = Vec::new();
+    let mut cards = Vec::<usize>::new();
     for line in BufReader::new(stdin()).lines() {
         let line = line.unwrap();
         let (_, nums) = line.split_once(':').unwrap();
@@ -17,7 +17,7 @@ fn main() {
             .split_whitespace()
             .map(|s| s.parse::<u64>().unwrap())
             .collect::<HashSet<u64>>();
-        let mut count = 0;
+        let mut count = 0usize;
         for num in own_nums {
             if win_nums.contains(&num) {
                 count += 1;
@@ -25,5 +25,16 @@ fn main() {
         }
         cards.push(count);
     }
-    println!("{cards:?}")
+    let mut copies = vec![0; cards.len()];
+    let mut sum = 0;
+    for i in 0..cards.len() {
+        let start = i + 1;
+        let end = (i + cards[i]).min(cards.len() - 1);
+        let instances = 1 + copies[i];
+        for j in start..=end {
+            copies[j] += instances;
+        }
+        sum += instances;
+    }
+    println!("{}", sum);
 }
